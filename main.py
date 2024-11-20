0fda392c4d4f965e4298d6e06215ddc058e28283
andrey.zelenyy <andrey.zelenyy@south.rt.ru>
2024-11-20 12:54:57 +0300

    Работа с WebSocket

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import uvicorn
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from starlette.websockets import WebSocketDisconnect
 
 from db import create_tables, get_engine
 from routers import user, game
+from websocket import ws
 
 app = FastAPI()
 
@@ -22,30 +21,14 @@ app.add_middleware(CORSMiddleware,
 
 app.include_router(user.router)
 app.include_router(game.router)
+app.include_router(ws.router)
 
 
 @app.get("/health")
 async def health_check():
     return {"status": "ok"}
 
+
 @app.post("/createTables")
 async def health_check():
     create_tables(get_engine())
-
-
-@app.websocket("/ws")
-async def websocket_endpoint(w: WebSocket):
-    await w.accept()
-    try:
-        while True:
-            data = await w.receive_json()
-            print('data:', data)
-            await w.send_text(f"Message text was: {data}")
-    except WebSocketDisconnect:
-        print('Disconnected')
-
-
-# if __name__ == "__main__":
-#     create_tables(get_engine())
-
-    # uvicorn.run(app, host="0.0.0.0", port=5000)
--- websocket/ws.py
+++ websocket/ws.py
@@ -1,13 +1,20 @@
-# from fastapi import WebSocket
-#
-# from main import app
-#
-#
-# @app.websocket("/ws")
-# async def websocket_endpoint(w: WebSocket):
-#     print('##### 1111 ######')
-#     await w.accept()
-#     print('##### 2222 ######')
-#     while True:
-#         data = await w.receive_text()
-#         await w.send_text(f"Message text was: {data}")
+from fastapi import WebSocket, APIRouter
+from starlette.websockets import WebSocketDisconnect
+
+
+router = APIRouter(
+    tags=["WebSocket"],
+    responses={404: {"description": "Not found"}},
+)
+
+
+@router.websocket_route("/ws")
+async def websocket_endpoint(w: WebSocket):
+    await w.accept()
+    try:
+        while True:
+            data = await w.receive_json()
+            print('data:', data)
+            await w.send_text(f"Message text was: {data}")
+    except WebSocketDisconnect:
+        print('Disconnected')
