df85a914dd2729d45a5a9564f4d1e33993bab78f
andrey.zelenyy <andrey.zelenyy@south.rt.ru>
2024-11-22 18:45:57 +0300

    ConnectionManager refactoring

--- websocket/connection_manager.py
+++ websocket/connection_manager.py
@@ -8,11 +8,11 @@ class ConnectionManager:
         self.active_connections: dict[uuid.UUID, WebSocket] = {}
 
     async def connect(self, client_uuid: uuid.UUID, websocket: WebSocket):
-        await websocket.accept()
         self.active_connections[client_uuid] = websocket
 
-    def disconnect(self, client_uuid: uuid.UUID):
-        self.active_connections.pop(client_uuid)
+    def disconnect(self, websocket: WebSocket):
+        new_dict = {key: val for key, val in self.active_connections.items() if val != websocket}
+        self.active_connections = new_dict
 
     async def send_personal_message(self, message: str, client_uuid: uuid.UUID):
         if client_uuid in self.active_connections:
@@ -21,3 +21,6 @@ class ConnectionManager:
     async def broadcast(self, message: str):  # но использовать широковещательную рассылку пока не планировал
         for connection in self.active_connections.values():
             await connection.send_text(message)
+
+    def print_clients(self):
+        print('Clients: ', self.active_connections.keys())
--- websocket/ws.py
+++ websocket/ws.py
@@ -1,22 +1,28 @@
-from fastapi import WebSocket, APIRouter
-from starlette.websockets import WebSocketDisconnect
+import uuid
 
-from controllers.ws.user import create_user
+from fastapi import WebSocket, APIRouter, WebSocketDisconnect
+
+from controllers.ws.main_controller import process_data
+from websocket.connection_manager import ConnectionManager
 
 router = APIRouter(
     tags=["WebSocket"],
     responses={404: {"description": "Not found"}},
 )
 
+manager = ConnectionManager()
 
-@router.websocket_route("/ws")
-async def websocket_endpoint(w: WebSocket):
-    await w.accept()
+
+@router.websocket_route("/client/{client_uuid}/ws")
+async def websocket_endpoint(ws: WebSocket):
+    await manager.connect()
     try:
         while True:
-            data = await w.receive_json()
-            create_user(data)
-            print('data:', data)
-            await w.send_text(f"Message text was: {data}")
+            data_from_client = await ws.receive_json()
+            data_for_client = await process_data(data_from_client)
+            print('data_from_client:', data_from_client)
+            print('data_for_client:', data_for_client)
+            await ws.send_text(data_for_client)
     except WebSocketDisconnect:
-        print('Disconnected')
+        manager.disconnect(ws)
+        manager.print_clients()
