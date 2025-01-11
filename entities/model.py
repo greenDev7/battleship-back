32bc88b9cbc06e5c2f898776ae06e264c0cb3053
andrey.zelenyy <andrey.zelenyy@south.rt.ru>
2025-01-11 11:03:26 +0300

    Изменения в модели

--- entities/model.py
+++ entities/model.py
@@ -6,27 +6,16 @@ class Base(DeclarativeBase):
     pass
 
 
-# class TGame(Base):
-#     __tablename__ = "tgame"
-#
-#     id = mapped_column(Uuid, primary_key=True)
-#     dfcreated_on = mapped_column(DateTime)
-#     dffinished_on = mapped_column(DateTime)
-#     dfwinner = mapped_column(Uuid)
-#     dfloser = mapped_column(Uuid)
-#     dfgame_type = mapped_column(ForeignKey("tgame_type.id"))
-
-
 class TRivalCouple(Base):
     __tablename__ = "trival_couple"
 
     id = mapped_column(Uuid, primary_key=True)
     dfplayer1 = mapped_column(Uuid)
     dfplayer1_nickname = mapped_column(String(100))
-    dfplayer1_state = mapped_column(ForeignKey("tstate.id"))
+    dfplayer1_state = mapped_column(ForeignKey("tplayer_state.id"))
     dfplayer2 = mapped_column(Uuid)
     dfplayer2_nickname = mapped_column(String(100))
-    dfplayer2_state = mapped_column(ForeignKey("tstate.id"))
+    dfplayer2_state = mapped_column(ForeignKey("tplayer_state.id"))
     dfgame_type = mapped_column(ForeignKey("tgame_type.id"))
     dfcreated_on = mapped_column(DateTime)
     dffinished_on = mapped_column(DateTime)
@@ -36,11 +25,13 @@ class TGameType(Base):
     __tablename__ = "tgame_type"
 
     id = mapped_column(SmallInteger, primary_key=True)
+    dfname_en = mapped_column(String(30))
     dfname = mapped_column(String(30))
 
 
-class TState(Base):
-    __tablename__ = "tstate"
+class TPlayerState(Base):
+    __tablename__ = "tplayer_state"
 
     id = mapped_column(SmallInteger, primary_key=True)
+    dfname_en = mapped_column(String(30))
     dfname = mapped_column(String(30))
